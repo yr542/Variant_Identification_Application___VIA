5b97f09126dd7e487c533dece94ef554cf338654
Anthony Ozerov <anthony@ozerov.email>
2021-05-02 20:37:43 -0400

    add family.py

---
+++ family.py
@@ -0,0 +1,16 @@
+# Family class. Stores the strings and phenotype for every member of the family.
+
+class Family:
+    def __init__(self, ID):
+        self.ID = ID
+        self.siblings = []
+        self.mother=""
+        self.father=""
+        self.child=""
+
+# Sibling class. Stores the string, sex, and phenotype for a sibling.
+class Sibling:
+    def __init__(self, ID, sex, phen):
+        self.ID = ID
+        self.sex = sex
+        self.phen = phen
