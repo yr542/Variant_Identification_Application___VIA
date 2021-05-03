93099732a83fccf90d5eb07628b28904dce0492b
AlexandraShanabrook <83547551+AlexandraShanabrook@users.noreply.github.com>
2021-05-03 18:30:55 -0400

    Update models.py

--- models.py
+++ models.py
@@ -11,7 +11,8 @@
 # The functions should take in the variant data frame and a Family object,
 # and output a data frame of possible variants.
 
-import filters.py
+from filters import *
+import pandas as pd
 
 #add_columns adds three columns to the dataFrame df containing info to be outputted for
 #candidate variants for a specific Family object fam and the relevant inheritance
@@ -19,34 +20,33 @@ import filters.py
 def add_columns(df, fam, modelno):
     df.insert(0, "inh model", modelno)
     df.insert(1, "family", fam.ID)
-    df.insert(2, "sample", 'sampleno') #sampleno will be replaced with value(s) when location is known
+    df.insert(2, "sample", fam.child)
 
 #ad_model takes in a data frame and Family object and returns a new data frame
 #containing candidate variants
 def ad_model(df, fam):
+    min_allelic_depth = 1650 * 6
     numAffected = 0
-    newdf = pd.DataFrame.copy()  #deep copy of data frame so other models can use the same one (likely inefficient, stopgap for now)
-    filter_AF(newdf, .0005)
+    newdf = filter_AF_into_new_DataFrame(df, .0005)
     if fam.mother_phen == "Affected":
         numAffected += 1
         filter_zyg(newdf, fam.mother, "0/1")
-        filter_ADs(newdf, fam.mother, 'min allelic depth') #min allelic depth will be replaced with a number when known here and below
+        filter_ADs(newdf, fam.mother, min_allelic_depth)
     if fam.father_phen == "Affected":
         numAffected += 1
         filter_zyg(newdf, fam.father, "0/1")
-        filter_ADs(newdf, fam.father, 'min allelic depth')
+        filter_ADs(newdf, fam.father, min_allelic_depth)
     if fam.child != "":
         numAffected += 1
         filter_zyg(newdf, fam.child, "0/1")
-        filter_ADs(newdf, fam.child, 'min allelic depth')
+        filter_ADs(newdf, fam.child, min_allelic_depth)
     for sib in fam.siblings:
-        if sib.phen == "Affected"
+        if sib.phen == "Affected":
             numAffected += 1
             filter_zyg(newdf, sib.ID, "0/1")
-            filter_ADs(newdf, sib.ID, 'min allelic depth')
+            filter_ADs(newdf, sib.ID, min_allelic_depth)
     if numAffected <= 1:
         return pd.DataFrame() #returns an empty Data Frame if nothing should be output for this model
     else:
         add_columns(newdf, fam, 4)
         return newdf
-  #to ask in meeting: should it filter out variants where a person is 0/1 and not affected? Seems like yes but not in specs
