0031535dc146d90700ac08d7ee7061666575e28c
AlexandraShanabrook <83547551+AlexandraShanabrook@users.noreply.github.com>
2021-07-19 18:40:09 -0400

    Loads Test_Phen.txt (HPO numbers)

--- main.py
+++ main.py
@@ -9,18 +9,20 @@ if __name__ == '__main__':
     argp.add_argument('-d', '--data', default="Test_cleaned.txt")
     argp.add_argument('-o', '--output', default="filtered.csv")
     argp.add_argument('-f', '--family', default="")
+    argp.add_argument('-ph', '--phenfile', default="Test_Phen.txt")
 
     args = argp.parse_args()
 
-    pedDf = pd.read_csv(args.pedfile,sep='\t')
+    pedDf = pd.read_csv(args.pedfile, sep='\t')
+    phenDf = pd.read_csv(args.phenfile, sep='\t')
 
     families = {}
     for i in range(0, len(pedDf)):
-        #print(i)
+        # print(i)
         Fam_ID = pedDf["Family_ID"][i]
         if Fam_ID not in families.keys():
             fam = Family(Fam_ID)
-            families[Fam_ID]=fam
+            families[Fam_ID] = fam
         else:
             fam = families.get(Fam_ID)
 
@@ -41,24 +43,31 @@ if __name__ == '__main__':
         elif status == "Sibling":
             fam.siblings.append(newperson)
 
-
+    #loads HPO numbers
+    for j in families:
+        for i in range(0, len(phenDf)):
+            if phenDf["Family_ID"][i] == j:
+                hpo = phenDf["HPO"][i]
+                fam = families.get(j)
+                fam.HPO = hpo.split(',')
+                
     df = pd.read_csv(args.data, sep='\t')
-    
-    #csv with variants in one family
+
+    # csv with variants in one family
     if args.family != "":
         fam = families[args.family]
         fam_variants = df.copy()
         for person in fam.people:
             filt = filter_zyg if person.phen == "Unaffected" else exclude_zyg
             fam_variants = filt(fam_variants, person.ID, "0/0")
-        fam_variants.to_csv(fam.ID+".csv")
+        fam_variants.to_csv(fam.ID + ".csv")
 
-    #empty dataframe to hold output
+    # empty dataframe to hold output
     result = pd.DataFrame()
-    
+
     for fam in families.values():
 
-        #generate a list of subfamilies centered on each affected individual
+        # generate a list of subfamilies centered on each affected individual
         subfamilies = [fam]
         for person in fam.people:
             if person.affected and person != fam.child:
@@ -82,9 +91,9 @@ if __name__ == '__main__':
                         subfamily.people.append(p)
                 subfamilies.append(subfamily)
 
-        #empty dataframe for results in this family
+        # empty dataframe for results in this family
         famresult = pd.DataFrame()
-        #add model results for each subfamily
+        # add model results for each subfamily
         for subfam in subfamilies:
             famresult = pd.concat([famresult, ad_model(df, subfam)])
             famresult = pd.concat([famresult, ar_model(df, subfam)])
@@ -93,28 +102,28 @@ if __name__ == '__main__':
             famresult = pd.concat([famresult, de_novo_model(df, subfam)])
             famresult = pd.concat([famresult, cmpd_het_model(df, subfam)])
 
-        #combine multiple instances of the same variant into one row
-        famresult["loc"] = [chrom+str(start)+str(end) for chrom, start,end in zip(famresult['Chr'], famresult['Start'], famresult["End"])]
+        # combine multiple instances of the same variant into one row
+        famresult["loc"] = [chrom + str(start) + str(end) for chrom, start, end in
+                            zip(famresult['Chr'], famresult['Start'], famresult["End"])]
         uniquelocs = famresult["loc"].unique()
         combined = pd.DataFrame()
         for loc in uniquelocs:
-            rows = famresult[famresult["loc"]==loc]
+            rows = famresult[famresult["loc"] == loc]
             output = rows.head(1).copy()
             samplestring = ""
             for sample in rows["sample"]:
-                samplestring += sample+","
-            samplestring = samplestring[:-1] #remove last comma
+                samplestring += sample + ","
+            samplestring = samplestring[:-1]  # remove last comma
             modelstring = ""
             for model in rows["inh model"]:
-                modelstring += model+","
-            modelstring = modelstring[:-1] #remove last comma
+                modelstring += model + ","
+            modelstring = modelstring[:-1]  # remove last comma
             output["sample"] = [samplestring]
             output["inh model"] = [modelstring]
-            del(output["loc"])
-            combined = pd.concat([combined,output])
+            del (output["loc"])
+            combined = pd.concat([combined, output])
         result = pd.concat([result, combined])
-    
-    
+
     # organize result first by inh model and then by sample
     result = result.sort_values(['sample', 'inh model'])
     result.to_csv(args.output)
