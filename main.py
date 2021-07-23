e811bad12e9d07dc47e9633df112a443ffe0d54b
Anthony Ozerov <anthony@ozerov.email>
2021-07-24 02:12:58 +0300

    Implement filtering genes according to family phenotype

--- main.py
+++ main.py
@@ -2,54 +2,26 @@ import argparse
 import pandas as pd
 from family import *
 from models import *
+from utils import *
 
 if __name__ == '__main__':
     argp = argparse.ArgumentParser()
     argp.add_argument('-p', '--pedfile', default="Test_Ped.txt")
     argp.add_argument('-d', '--data', default="Test_cleaned.txt")
     argp.add_argument('-o', '--output', default="filtered.csv")
+    argp.add_argument('-op', '--output_phen', default="filtered_phen.csv")
     argp.add_argument('-f', '--family', default="")
     argp.add_argument('-ph', '--phenfile', default="Test_Phen.txt")
+    argp.add_argument('-m', '--mapfile', default="phenotype_to_genes.txt")
+    argp.add_argument('--nophen', default = False, action = 'store_true')
 
     args = argp.parse_args()
 
-    pedDf = pd.read_csv(args.pedfile, sep='\t')
-    phenDf = pd.read_csv(args.phenfile, sep='\t')
+    families = get_families(args.pedfile)
 
-    families = {}
-    for i in range(0, len(pedDf)):
-        # print(i)
-        Fam_ID = pedDf["Family_ID"][i]
-        if Fam_ID not in families.keys():
-            fam = Family(Fam_ID)
-            families[Fam_ID] = fam
-        else:
-            fam = families.get(Fam_ID)
-
-        ID = pedDf["individual_ID"][i]
-        status = pedDf["Status"][i]
-        sex = pedDf["Sex"][i]
-        phen = pedDf["Phenotype"][i]
-        newperson = Person(ID, sex, phen)
-        fam.people.append(newperson)
-        if status == "Father":
-            fam.father = newperson
-            fam.hasFather = True
-        elif status == "Mother":
-            fam.mother = newperson
-            fam.hasMother = True
-        elif status == "Child":
-            fam.child = newperson
-        elif status == "Sibling":
-            fam.siblings.append(newperson)
-
-    #loads HPO numbers
-    for j in families:
-        for i in range(0, len(phenDf)):
-            if phenDf["Family_ID"][i] == j:
-                hpo = phenDf["HPO"][i]
-                fam = families.get(j)
-                fam.HPO = hpo.split(',')
+    
+    if not args.nophen:
+        load_phen(families, args.phenfile, args.mapfile)
                 
     df = pd.read_csv(args.data, sep='\t')
 
@@ -62,34 +34,14 @@ if __name__ == '__main__':
             fam_variants = filt(fam_variants, person.ID, "0/0")
         fam_variants.to_csv(fam.ID + ".csv")
 
-    # empty dataframe to hold output
+
     result = pd.DataFrame()
+    result_p = pd.DataFrame()
 
     for fam in families.values():
 
         # generate a list of subfamilies centered on each affected individual
-        subfamilies = [fam]
-        for person in fam.people:
-            if person.affected and person != fam.child:
-                subfamily = Family(fam.ID)
-                subfamily.child = person
-                opposite = ""
-                if person == fam.father or person == fam.mother:
-                    opposite = fam.mother if person == fam.father else fam.father
-                if person in fam.siblings:
-                    if fam.hasFather:
-                        subfamily.father = fam.father
-                        subfamily.hasFather = True
-                    if fam.hasMother:
-                        subfamily.mother = fam.mother
-                        subfamily.hasMother = True
-                    newsibs = fam.siblings.copy()
-                    newsibs.remove(person)
-                    subfamily.siblings = newsibs + [fam.child]
-                for p in fam.people:
-                    if p != opposite:
-                        subfamily.people.append(p)
-                subfamilies.append(subfamily)
+        subfamilies = generate_subfamilies(fam)
 
         # empty dataframe for results in this family
         famresult = pd.DataFrame()
@@ -102,29 +54,20 @@ if __name__ == '__main__':
             famresult = pd.concat([famresult, de_novo_model(df, subfam)])
             famresult = pd.concat([famresult, cmpd_het_model(df, subfam)])
 
-        # combine multiple instances of the same variant into one row
-        famresult["loc"] = [chrom + str(start) + str(end) for chrom, start, end in
-                            zip(famresult['Chr'], famresult['Start'], famresult["End"])]
-        uniquelocs = famresult["loc"].unique()
-        combined = pd.DataFrame()
-        for loc in uniquelocs:
-            rows = famresult[famresult["loc"] == loc]
-            output = rows.head(1).copy()
-            samplestring = ""
-            for sample in rows["sample"]:
-                samplestring += sample + ","
-            samplestring = samplestring[:-1]  # remove last comma
-            modelstring = ""
-            for model in rows["inh model"]:
-                modelstring += model + ","
-            modelstring = modelstring[:-1]  # remove last comma
-            output["sample"] = [samplestring]
-            output["inh model"] = [modelstring]
-            del (output["loc"])
-            combined = pd.concat([combined, output])
+        combined = combine_duplicates(famresult)
         result = pd.concat([result, combined])
 
-    # organize result first by inh model and then by sample
+        if not args.nophen:
+            combined = filter_phen(combined, fam)
+            result_p = pd.concat([result_p, combined])
+
+    # organize result first by sample and then by inh model
     result = result.sort_values(['sample', 'inh model'])
+    #save result
     result.to_csv(args.output)
     print(result)
+
+    if not args.nophen:
+        result_p = result_p.sort_values(['family','phens_matched','sample'])
+        result_p.to_csv(args.output_phen)
+        print(result_p)
